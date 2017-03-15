0fa8051f5db7affb69576499694fa8471d8bb9c1
faithngetich <faith.ngetich@andela.com>
2017-03-15 16:04:46 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from tweet import *
 
 class TwitterDataTestCase(unittest.TestCase):
     def test_clean_data(self):
-        data = ['gold', 'car', 'jew', 'great, 'http']
+        data = ['gold', 'car', 'jew', 'great', 'http']
         self.assertEqual(clean_text(data), ['gold', 'car', 'jew', 'great',])
 
 if __name__ == '__main__':
