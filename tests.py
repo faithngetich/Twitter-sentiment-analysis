042b9f0c4117fd630c23792930aa351a796e59db
faithngetich <faith.ngetich@andela.com>
2017-03-15 15:52:59 +0300

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+from tweet import *
+
+
+class TwitterDataTestCase(unittest.TestCase):
+    def test_clean_data(self):
+        data = ['gold', 'car', 'jew', 'great, 'http']
+        self.assertEqual(clean_text(data), ['gold', 'car', 'jew', 'great',])
+
+if __name__ == '__main__':
+    unittest.main()
