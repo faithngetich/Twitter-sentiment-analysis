436dd2270894ad8168aea53952d44c5a0dac8dc8
faithngetich <faithngetich188@gmail.com>
2023-01-07 18:00:50 +0300

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -19,10 +19,6 @@ from analysis import alchemy
 
 
 # twitter access tokens (security keys from the twitter application)
-access_token = "3228931192-dgwDS0IabNwy3ujynR8BMq0k9Mky6dp3qErmydr"
-access_token_secret = "sM067uSqM5XLu3gR6d2FxAfahxhxvBuxMuMnm2YqJjy9C"
-consumer_key = "VHmz0kLyncU4mV2Idre5VQKyR"
-consumer_secret = "XeT1YP657uPacDMPcAbMv94AwzaclZcJLF1J29xTeOzgvJGWam"
 
 # twitter OAuth authentication
 # Into OAuthHandler instance, we pass our consumer token and secret
